d24b474a7ab64987f35a60ff8c0f012b81ee0665
Andreas Petersson <anpe9592@users.noreply.github.com>
2015-04-01 16:31:38 +0200

    problem12

--- problem12.py
+++ problem12.py
@@ -1,2 +1,23 @@
 # problem12.py
 
+import math
+
+x = 0
+z = 0
+n = 1
+
+i = True
+while i != False:
+    z = n * (n + 1) / 2
+    x = 2
+    
+    y = int(math.sqrt(z))
+    for k in range(2, y):
+        if z % k == 0:
+            x += 2
+        if x == 500:
+            i = False
+
+    n += 1
+
+print(z)
