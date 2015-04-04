1eaac725e10ff8ac289a8163d651d59bf3d1a3a4
Andreas Petersson <anpe9592@users.noreply.github.com>
2015-04-04 21:22:35 +0200

    problem14 update

--- problem14.py
+++ problem14.py
@@ -1,17 +1,25 @@
 # problem14.py
 
-x = 13
-
+mil = 1000001
 z = 0
+u = 0
+for x in range(mil):
+    print("start: ", x)
+    i = x
+    y = 0
+    while x != 1:
+        if (x % 2) == 1 or x == 0:
+            x = int(3 * x + 1)
+        else:
+            x = int(x / 2)
 
-while x != 1:
-    if (x % 2) == 1 or x == 0:
-        x = int(3 * x + 1)
-    else:
-        x = int(x / 2)
+        print(x)
 
-    print(x)
+        y += 1
 
-    z += 1
-        
-print(z)
+    if z < y:
+        z = y
+        u = i
+
+print("Count: ", z)
+print("Number: ", u)
