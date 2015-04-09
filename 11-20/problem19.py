86410209e6288dd04d0c42694634812efbecdfea
Andreas Petersson <anpe9592@users.noreply.github.com>
2015-04-09 09:30:54 +0200

    problem19 solved

--- 11-20/problem19.py
+++ 11-20/problem19.py
@@ -1,2 +1,12 @@
 # problem19.py
-# 171
+
+import datetime
+
+sun = 0
+
+for y in range(1901, 2001):
+    for m in range(1, 13):
+        if datetime.date(y, m, 1).isoweekday() == 7:
+            sun += 1
+
+print(sun)
